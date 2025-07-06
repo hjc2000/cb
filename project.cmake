521479062ee904770cb0e548b82f55f79b933198
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-07-06 23:49:07 +0800

    将测试程序从C迁移到C++并添加Hello World示例 将main.c改为main.cpp并实现基础输出功能

--- project.cmake
+++ project.cmake
@@ -5,7 +5,7 @@ target_import_src(${ProjectName})
 # 添加测试程序
 if(platform STREQUAL "msys")
 	set(exe_name "test")
-	add_executable(${exe_name} ${CMAKE_CURRENT_SOURCE_DIR}/exe/main.c)
+	add_executable(${exe_name} ${CMAKE_CURRENT_SOURCE_DIR}/exe/main.cpp)
 	target_link_libraries(${exe_name} ${ProjectName})
 	target_install(${exe_name})
 endif()
