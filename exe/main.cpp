ea39683a471d9a2457be5e6356a88b80921161eb
hjc2000 <hjc.666@foxmail.com>
2025-07-07 14:40:20 +0800

    "新增大端和小端远程位转换器演示程序 实现并演示大端和小端远程位转换器的使用，展示数值转十六进制字节数组功能"

--- exe/main.cpp
+++ exe/main.cpp
@@ -1,14 +1,35 @@
-#include "cb/math/counter.h"
+#include "base/string/define.h"
+#include "base/string/ToHexString.h"
+#include "cb/bit/big_endian_remote_bit_converter.h"
+#include "cb/bit/little_endian_remote_bit_converter.h"
+#include <cstdint>
 #include <iostream>
 
 int main()
 {
-	cb_counter_32 counter{};
-	cb_counter_32_initialize(&counter, 0, 9);
-
-	for (int i = 0; i < 30; i++)
 	{
-		std::cout << cb_counter_32_current_value(&counter) << std::endl;
-		cb_counter_32_add(&counter, 1);
+		std::cout << CODE_POS_STR;
+		uint64_t num = 0x0123456789ABCDEF;
+		uint8_t arr[sizeof(num)]{};
+		cb_big_endian_remote_bit_converter_get_bytes_from_uint64(num, arr);
+		base::ToHexStringOption option{};
+		option.width = 2;
+		for (uint8_t num : arr)
+		{
+			std::cout << base::ToHexString(num, option) << std::endl;
+		}
+	}
+
+	{
+		std::cout << CODE_POS_STR;
+		uint64_t num = 0x0123456789ABCDEF;
+		uint8_t arr[sizeof(num)]{};
+		cb_little_endian_remote_bit_converter_get_bytes_from_uint64(num, arr);
+		base::ToHexStringOption option{};
+		option.width = 2;
+		for (uint8_t num : arr)
+		{
+			std::cout << base::ToHexString(num, option) << std::endl;
+		}
 	}
 }
--- project.cmake
+++ project.cmake
@@ -2,6 +2,11 @@
 add_library(${ProjectName} STATIC)
 target_import_src(${ProjectName})
 
+if(platform STREQUAL "msys")
+	target_import_base(${ProjectName} PUBLIC)
+endif()
+
+
 # 添加测试程序
 if(platform STREQUAL "msys")
 	set(exe_name "test")
