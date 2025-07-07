8f327ef257410b5545aee17b7294c36e695604af
hjc2000 <hjc.666@foxmail.com>
2025-07-07 09:12:07 +0800

    "将位转换器函数实现内联化 移除bit_converter.c实现文件，将所有转换函数内联化到头文件中"

--- include/cb/bit_converter.h
+++ include/cb/bit_converter.h
@@ -89,18 +89,46 @@ __cb_force_inline void cb_bit_converter_get_bytes_from_uint16(uint16_t value, ui
 	memcpy(out_buffer + offset, buffer, sizeof(value));
 }
 
-void cb_bit_converter_get_bytes_from_int16(int16_t value, uint8_t *out_buffer, int32_t offset);
+__cb_force_inline void cb_bit_converter_get_bytes_from_int16(int16_t value, uint8_t *out_buffer, int32_t offset)
+{
+	uint8_t const *buffer = (uint8_t const *)(&value);
+	memcpy(out_buffer + offset, buffer, sizeof(value));
+}
 
-void cb_bit_converter_get_bytes_from_uint32(uint32_t value, uint8_t *out_buffer, int32_t offset);
+__cb_force_inline void cb_bit_converter_get_bytes_from_uint32(uint32_t value, uint8_t *out_buffer, int32_t offset)
+{
+	uint8_t const *buffer = (uint8_t const *)(&value);
+	memcpy(out_buffer + offset, buffer, sizeof(value));
+}
 
-void cb_bit_converter_get_bytes_from_int32(int32_t value, uint8_t *out_buffer, int32_t offset);
+__cb_force_inline void cb_bit_converter_get_bytes_from_int32(int32_t value, uint8_t *out_buffer, int32_t offset)
+{
+	uint8_t const *buffer = (uint8_t const *)(&value);
+	memcpy(out_buffer + offset, buffer, sizeof(value));
+}
 
-void cb_bit_converter_get_bytes_from_uint64(uint64_t value, uint8_t *out_buffer, int32_t offset);
+__cb_force_inline void cb_bit_converter_get_bytes_from_uint64(uint64_t value, uint8_t *out_buffer, int32_t offset)
+{
+	uint8_t const *buffer = (uint8_t const *)(&value);
+	memcpy(out_buffer + offset, buffer, sizeof(value));
+}
 
-void cb_bit_converter_get_bytes_from_int64(int64_t value, uint8_t *out_buffer, int32_t offset);
+__cb_force_inline void cb_bit_converter_get_bytes_from_int64(int64_t value, uint8_t *out_buffer, int32_t offset)
+{
+	uint8_t const *buffer = (uint8_t const *)(&value);
+	memcpy(out_buffer + offset, buffer, sizeof(value));
+}
 
-void cb_bit_converter_get_bytes_from_float(float value, uint8_t *out_buffer, int32_t offset);
+__cb_force_inline void cb_bit_converter_get_bytes_from_float(float value, uint8_t *out_buffer, int32_t offset)
+{
+	uint8_t const *buffer = (uint8_t const *)(&value);
+	memcpy(out_buffer + offset, buffer, sizeof(value));
+}
 
-void cb_bit_converter_get_bytes_from_double(double value, uint8_t *out_buffer, int32_t offset);
+__cb_force_inline void cb_bit_converter_get_bytes_from_double(double value, uint8_t *out_buffer, int32_t offset)
+{
+	uint8_t const *buffer = (uint8_t const *)(&value);
+	memcpy(out_buffer + offset, buffer, sizeof(value));
+}
 
 /* #endregion */
