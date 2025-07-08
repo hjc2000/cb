7d96c8521ff7be893b303902af14789d33e6d265
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-07-08 23:34:45 +0800

    "重构64位线性插值器实现 将int64_linear_interpolator的初始化及更新方法从头文件移至源文件实现"

--- include/cb/math/int64_linear_interpolator.c
+++ include/cb/math/int64_linear_interpolator.c
@@ -1,1 +1,46 @@
 #include "int64_linear_interpolator.h" // IWYU pragma: keep
+
+void cb_int64_linear_interpolator_initialize(cb_int64_linear_interpolator *self,
+											 int64_t current_value,
+											 int64_t end_value,
+											 int64_t step_length)
+{
+	self->_current_value = current_value;
+	self->_end_value = end_value;
+
+	if (step_length < 0)
+	{
+		step_length = -step_length;
+	}
+
+	self->_step_length = step_length;
+}
+
+int64_t cb_int64_linear_interpolator_update(cb_int64_linear_interpolator *self)
+{
+	int64_t error = self->_end_value - self->_current_value;
+	if (error < 0)
+	{
+		error = -error;
+	}
+
+	if (error < self->_step_length)
+	{
+		// 差距小于 1 个步长，直接赋值为结束值。
+		self->_current_value = self->_end_value;
+		return self->_current_value;
+	}
+
+	// 差距大于 1 个步长。
+	// 从正向和负向逼近结束值。
+	if (self->_current_value < self->_end_value)
+	{
+		self->_current_value += self->_step_length;
+	}
+	else if (self->_current_value > self->_end_value)
+	{
+		self->_current_value -= self->_step_length;
+	}
+
+	return self->_current_value;
+}
