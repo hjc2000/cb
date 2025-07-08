943546d90b9800280da5d1f9dc304a7a4225b46f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-07-08 23:11:19 +0800

    "新增线性插值器访问和更新方法 实现int64_linear_interpolator的当前值、结束值和步长访问器及设置方法，并添加线性插值更新逻辑"

--- include/cb/math/int64_linear_interpolator.h
+++ include/cb/math/int64_linear_interpolator.h
@@ -30,6 +30,65 @@ extern "C"
 		self->_step_length = step_length;
 	}
 
+	__cb_force_inline int64_t cb_int64_linear_interpolator_current_value(cb_int64_linear_interpolator const *self)
+	{
+		return self->_current_value;
+	}
+
+	__cb_force_inline void cb_int64_linear_interpolator_set_current_value(cb_int64_linear_interpolator *self, int64_t value)
+	{
+		self->_current_value = value;
+	}
+
+	__cb_force_inline int64_t cb_int64_linear_interpolator_end_value(cb_int64_linear_interpolator const *self)
+	{
+		return self->_end_value;
+	}
+
+	__cb_force_inline void cb_int64_linear_interpolator_set_end_value(cb_int64_linear_interpolator *self, int64_t value)
+	{
+		self->_end_value = value;
+	}
+
+	__cb_force_inline int64_t cb_int64_linear_interpolator_step_length(cb_int64_linear_interpolator const *self)
+	{
+		return self->_step_length;
+	}
+
+	__cb_force_inline void cb_int64_linear_interpolator_set_step_length(cb_int64_linear_interpolator *self, int64_t value)
+	{
+		self->_step_length = value;
+	}
+
+	__cb_force_inline int64_t cb_int64_linear_interpolator_update(cb_int64_linear_interpolator *self)
+	{
+		int64_t error = self->_end_value - self->_current_value;
+		if (error < 0)
+		{
+			error = -error;
+		}
+
+		if (error < self->_step_length)
+		{
+			// 差距小于 1 个步长，直接赋值为结束值。
+			self->_current_value = self->_end_value;
+			return self->_current_value;
+		}
+
+		// 差距大于 1 个步长。
+		// 从正向和负向逼近结束值。
+		if (self->_current_value < self->_end_value)
+		{
+			self->_current_value += self->_step_length;
+		}
+		else if (self->_current_value > self->_end_value)
+		{
+			self->_current_value -= self->_step_length;
+		}
+
+		return self->_current_value;
+	}
+
 #ifdef __cplusplus
 }
 #endif
