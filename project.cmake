a45a1cee9559ccb9eca11b0f997fbd4843871706
hjc2000 <hjc.666@foxmail.com>
2025-08-13 15:11:36 +0800

    扩展平台支持到msys-clang 修改project.cmake条件判断，同时支持msys和msys-clang平台

--- project.cmake
+++ project.cmake
@@ -2,13 +2,15 @@
 add_library(${ProjectName} STATIC)
 target_import_src(${ProjectName})
 
-if(platform STREQUAL "msys")
+if(("${platform}" STREQUAL "msys") OR
+   ("${platform}" STREQUAL "msys-clang"))
 	target_import_base(${ProjectName} PUBLIC)
 endif()
 
 
 # 添加测试程序
-if(platform STREQUAL "msys")
+if(("${platform}" STREQUAL "msys") OR
+   ("${platform}" STREQUAL "msys-clang"))
 	set(exe_name "test")
 	add_executable(${exe_name} ${CMAKE_CURRENT_SOURCE_DIR}/exe/main.cpp)
 	target_link_libraries(${exe_name} ${ProjectName})
