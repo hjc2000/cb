540f64601e718f1efd8720ab60320e6611808199
hjc2000 <hjc.666@foxmail.com>
2025-08-14 11:56:34 +0800

    为cb_counter_32添加随机加减测试用例并优化输出代码

--- test/cb/test/test_counter_32.cpp
+++ test/cb/test/test_counter_32.cpp
@@ -1,6 +1,5 @@
 #include "test_counter_32.h" // IWYU pragma: keep
 #include "cb/math/cb_counter_32.h"
-#include <cstdint>
 #include <iostream>
 
 void cb::test::test_counter_32()
@@ -13,8 +12,7 @@ void cb::test::test_counter_32()
 		cb_counter_32_initialize(&counter, 0, 9);
 		for (int i = 0; i < 20; i++)
 		{
-			uint32_t value = cb_counter_32_current_value(&counter);
-			std::cout << value << ", ";
+			std::cout << cb_counter_32_current_value(&counter) << ", ";
 			if ((i + 1) % 10 == 0)
 			{
 				std::cout << std::endl;
@@ -34,8 +32,7 @@ void cb::test::test_counter_32()
 		cb_counter_32_initialize(&counter, 0, 9);
 		for (int i = 0; i < 20; i++)
 		{
-			uint32_t value = cb_counter_32_current_value(&counter);
-			std::cout << value << ", ";
+			std::cout << cb_counter_32_current_value(&counter) << ", ";
 			if ((i + 1) % 10 == 0)
 			{
 				std::cout << std::endl;
@@ -43,6 +40,22 @@ void cb::test::test_counter_32()
 
 			cb_counter_32_decrement(&counter);
 		}
+
+		std::cout << std::endl;
+	}
+
+	{
+		std::cout << std::endl
+				  << "测试随机加减: " << std::endl;
+
+		cb_counter_32 counter;
+		cb_counter_32_initialize(&counter, 0, 9);
+
+		cb_counter_32_add(&counter, 107);
+		std::cout << cb_counter_32_current_value(&counter) << std::endl;
+
+		cb_counter_32_subtract(&counter, 107);
+		std::cout << cb_counter_32_current_value(&counter) << std::endl;
 
 		std::cout << std::endl;
 	}
