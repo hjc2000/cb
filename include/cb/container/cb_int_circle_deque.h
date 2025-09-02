7a0f9d668d5a8ac990a77fb1ef278da78a46971f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-09-02 18:35:00 +0800

    refactor(cb_int_circle_deque): 将函数实现改为内联函数
    - 将 cb_int_circle_deque 的各个函数实现改为内联函数
    - 在头文件中直接定义内联函数，以提高性能
    - 删除了原来的函数实现文件

--- include/cb/container/cb_int_circle_deque.h
+++ include/cb/container/cb_int_circle_deque.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "cb/cb_define.h"
 #include <stdbool.h>
 
 #define __template_cb_circle_deque_element_type int
@@ -28,7 +29,10 @@ extern "C"
 	/// @param self
 	/// @return
 	///
-	void cb_int_circle_deque_initialize(cb_int_circle_deque *self);
+	__cb_force_inline void cb_int_circle_deque_initialize(cb_int_circle_deque *self)
+	{
+		cb_circle_deque_initialize(&self->_deque);
+	}
 
 	///
 	/// @brief 队列中的元素个数。
@@ -36,7 +40,10 @@ extern "C"
 	/// @param self
 	/// @return
 	///
-	int cb_int_circle_deque_count(cb_int_circle_deque *self);
+	__cb_force_inline int cb_int_circle_deque_count(cb_int_circle_deque *self)
+	{
+		return cb_circle_deque_count(&self->_deque);
+	}
 
 	///
 	/// @brief 队列为空。
@@ -44,7 +51,10 @@ extern "C"
 	/// @param self
 	/// @return
 	///
-	bool cb_int_circle_deque_is_empty(cb_int_circle_deque *self);
+	__cb_force_inline bool cb_int_circle_deque_is_empty(cb_int_circle_deque *self)
+	{
+		return cb_circle_deque_is_empty(&self->_deque);
+	}
 
 	///
 	/// @brief 向队列末尾添加元素。
@@ -53,7 +63,10 @@ extern "C"
 	/// @param value
 	/// @return 添加成功返回 true, 添加失败返回 false.
 	///
-	bool cb_int_circle_deque_push_back(cb_int_circle_deque *self, int *value);
+	__cb_force_inline bool cb_int_circle_deque_push_back(cb_int_circle_deque *self, int *value)
+	{
+		return cb_circle_deque_push_back(&self->_deque, value);
+	}
 
 	///
 	/// @brief 向队列头部添加元素。
@@ -62,7 +75,10 @@ extern "C"
 	/// @param value
 	/// @return 添加成功返回 true, 添加失败返回 false.
 	///
-	bool cb_int_circle_deque_push_front(cb_int_circle_deque *self, int *value);
+	__cb_force_inline bool cb_int_circle_deque_push_front(cb_int_circle_deque *self, int *value)
+	{
+		return cb_circle_deque_push_front(&self->_deque, value);
+	}
 
 	///
 	/// @brief 弹出队列末尾元素。
@@ -71,7 +87,10 @@ extern "C"
 	/// @param out
 	/// @return
 	///
-	bool cb_int_circle_deque_pop_back(cb_int_circle_deque *self, int *out);
+	__cb_force_inline bool cb_int_circle_deque_pop_back(cb_int_circle_deque *self, int *out)
+	{
+		return cb_circle_deque_pop_back(&self->_deque, out);
+	}
 
 	///
 	/// @brief 弹出队列头部元素。
@@ -80,7 +99,10 @@ extern "C"
 	/// @param out
 	/// @return
 	///
-	bool cb_int_circle_deque_pop_front(cb_int_circle_deque *self, int *out);
+	__cb_force_inline bool cb_int_circle_deque_pop_front(cb_int_circle_deque *self, int *out)
+	{
+		return cb_circle_deque_pop_front(&self->_deque, out);
+	}
 
 	///
 	/// @brief 获取队列中指定索引的元素。
@@ -90,7 +112,10 @@ extern "C"
 	///
 	/// @return 如果指定索引处有元素，则返回该元素的指针，否则返回空指针。
 	///
-	int *cb_int_circle_deque_get(cb_int_circle_deque *self, int index);
+	__cb_force_inline int *cb_int_circle_deque_get(cb_int_circle_deque *self, int index)
+	{
+		return cb_circle_deque_get(&self->_deque, index);
+	}
 
 	///
 	/// @brief 清空队列。
@@ -98,7 +123,10 @@ extern "C"
 	/// @param self
 	/// @return
 	///
-	void cb_int_circle_deque_clear(cb_int_circle_deque *self);
+	__cb_force_inline void cb_int_circle_deque_clear(cb_int_circle_deque *self)
+	{
+		cb_circle_deque_clear(&self->_deque);
+	}
 
 #ifdef __cplusplus
 }
