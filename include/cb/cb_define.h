7392f0417aa142294adbcca8dad61b003be79c34
hjc2000 <hjc.666@foxmail.com>
2025-09-29 14:07:55 +0800

    __cb_padding

--- include/cb/cb_define.h
+++ include/cb/cb_define.h
@@ -1,5 +1,11 @@
 #pragma once
 
+// IWYU pragma: begin_keep
+
+#include <stdint.h>
+
+// IWYU pragma: end_keep
+
 #ifdef __cplusplus
 extern "C"
 {
@@ -8,6 +14,9 @@ extern "C"
 #define __cb_force_inline inline __attribute__((always_inline))
 
 #define __cb_weak __attribute__((weak))
+
+#define __cb_padding(N) \
+	uint32_t __cb_padding_##__COUNTER__[N]
 
 #ifdef __cplusplus
 }
