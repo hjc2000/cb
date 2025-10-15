663154075ecb499ff5e72a001595f8a73e7e0af1
hjc2000 <hjc.666@foxmail.com>
2025-10-15 13:40:00 +0800

    ``` refactor(bit): 重构 AutoBitConverter 类的命名空间结构
    将 cb::bit_converter::AutoBitConverter 类移出 bit_converter 命名空间，
    直接放置于 cb 命名空间下，简化类型使用路径。
    
    同时修正 FromByte 方法名为 FromBytes，使其语义更准确。
    ```

--- include/cb/bit/cb_auto_bit_converter.h
+++ include/cb/bit/cb_auto_bit_converter.h
@@ -8,70 +8,67 @@
 
 namespace cb
 {
-	namespace bit_converter
+	class AutoBitConverter
 	{
-		class AutoBitConverter
+	private:
+		cb::endian::Endian _remote_endian{};
+
+		constexpr bool ShouldReverse() const
 		{
-		private:
-			cb::endian::Endian _remote_endian{};
+			return cb::endian::NativeEndian() != _remote_endian;
+		}
 
-			constexpr bool ShouldReverse() const
+	public:
+		constexpr AutoBitConverter(cb::endian::Endian remote_endian)
+		{
+			_remote_endian = remote_endian;
+		}
+
+		///
+		/// @brief 从字节缓冲区中反序列化出对象。
+		///
+		/// @note 会自动根据本机字节序和远端字节序进行字节序转换。
+		///
+		/// @param buffer
+		/// @return
+		///
+		template <typename ReturnType>
+		ReturnType FromBytes(cb::ReadOnlySpan const &span) const
+		{
+			ReturnType ret = cb::bit_converter::FromBytes<ReturnType>(span);
+
+			if (ShouldReverse())
 			{
-				return cb::endian::NativeEndian() != _remote_endian;
+				uint8_t *p = reinterpret_cast<uint8_t *>(&ret);
+				std::reverse(p, p + sizeof(ReturnType));
 			}
 
-		public:
-			constexpr AutoBitConverter(cb::endian::Endian remote_endian)
+			return ret;
+		}
+
+		///
+		/// @brief 将对象序列化到字节缓冲区中。
+		///
+		/// @note 会自动根据本机字节序和远端字节序进行字节序转换。
+		///
+		/// @param value
+		/// @param out_buffer
+		/// @return
+		///
+		template <typename ValueType>
+		void GetBytes(ValueType value, uint8_t *out_buffer) const
+		{
+			cb::bit_converter::GetBytes<ValueType>(value, out_buffer);
+
+			if (ShouldReverse())
 			{
-				_remote_endian = remote_endian;
+				std::reverse(out_buffer, out_buffer + sizeof(ValueType));
 			}
+		}
+	};
 
-			///
-			/// @brief 从字节缓冲区中反序列化出对象。
-			///
-			/// @note 会自动根据本机字节序和远端字节序进行字节序转换。
-			///
-			/// @param buffer
-			/// @return
-			///
-			template <typename ReturnType>
-			ReturnType FromByte(cb::ReadOnlySpan const &span) const
-			{
-				ReturnType ret = cb::bit_converter::FromBytes<ReturnType>(span);
+	constexpr cb::AutoBitConverter big_endian_remote_converter{cb::endian::Endian::BigEndian};
 
-				if (ShouldReverse())
-				{
-					uint8_t *p = reinterpret_cast<uint8_t *>(&ret);
-					std::reverse(p, p + sizeof(ReturnType));
-				}
+	constexpr cb::AutoBitConverter little_endian_remote_converter{cb::endian::Endian::LittleEndian};
 
-				return ret;
-			}
-
-			///
-			/// @brief 将对象序列化到字节缓冲区中。
-			///
-			/// @note 会自动根据本机字节序和远端字节序进行字节序转换。
-			///
-			/// @param value
-			/// @param out_buffer
-			/// @return
-			///
-			template <typename ValueType>
-			void GetBytes(ValueType value, uint8_t *out_buffer) const
-			{
-				cb::bit_converter::GetBytes<ValueType>(value, out_buffer);
-
-				if (ShouldReverse())
-				{
-					std::reverse(out_buffer, out_buffer + sizeof(ValueType));
-				}
-			}
-		};
-
-		constexpr cb::bit_converter::AutoBitConverter big_endian_remote_converter{cb::endian::Endian::BigEndian};
-
-		constexpr cb::bit_converter::AutoBitConverter little_endian_remote_converter{cb::endian::Endian::LittleEndian};
-
-	} // namespace bit_converter
 } // namespace cb
