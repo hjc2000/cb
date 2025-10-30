61c2bf027a175676c8c6dffb09e63d2924ad63ba
hjc2000 <hjc.666@foxmail.com>
2025-10-30 16:17:58 +0800

    namespace

--- include/cb/bit/cb_bit_converte.h
+++ include/cb/bit/cb_bit_converte.h
@@ -6,45 +6,48 @@
 #include <cstdint>
 #include <stdint.h>
 
-namespace cb::bit_converte
+namespace cb
 {
-	///
-	/// @brief 从字节缓冲区中反序列化出指定类型对象。
-	///
-	/// @param span
-	/// @return
-	///
-	template <typename ReturnType>
-	ReturnType FromBytes(cb::ReadOnlySpan const &span)
+	namespace bit_converte
 	{
-		__cb_assert(span.Size() == static_cast<int64_t>(sizeof(ReturnType)), "传入的 span 大小不符。");
+		///
+		/// @brief 从字节缓冲区中反序列化出指定类型对象。
+		///
+		/// @param span
+		/// @return
+		///
+		template <typename ReturnType>
+		ReturnType FromBytes(cb::ReadOnlySpan const &span)
+		{
+			__cb_assert(span.Size() == static_cast<int64_t>(sizeof(ReturnType)), "传入的 span 大小不符。");
 
-		ReturnType ret;
+			ReturnType ret;
 
-		std::copy(span.Buffer(),
-				  span.Buffer() + sizeof(ret),
-				  reinterpret_cast<uint8_t *>(&ret));
+			std::copy(span.Buffer(),
+					  span.Buffer() + sizeof(ret),
+					  reinterpret_cast<uint8_t *>(&ret));
 
-		return ret;
-	}
+			return ret;
+		}
 
-	///
-	/// @brief 将指定类型对象序列化到字节缓冲区中。
-	///
-	/// @param value
-	/// @param span
-	/// @return
-	///
-	template <typename ValueType>
-	void GetBytes(ValueType value, cb::Span const &span)
-	{
-		__cb_assert(span.Size() == static_cast<int64_t>(sizeof(ValueType)), "传入的 span 大小不符。");
+		///
+		/// @brief 将指定类型对象序列化到字节缓冲区中。
+		///
+		/// @param value
+		/// @param span
+		/// @return
+		///
+		template <typename ValueType>
+		void GetBytes(ValueType value, cb::Span const &span)
+		{
+			__cb_assert(span.Size() == static_cast<int64_t>(sizeof(ValueType)), "传入的 span 大小不符。");
 
-		uint8_t const *buffer = reinterpret_cast<uint8_t const *>(&value);
+			uint8_t const *buffer = reinterpret_cast<uint8_t const *>(&value);
 
-		std::copy(buffer,
-				  buffer + sizeof(value),
-				  span.Buffer());
-	}
+			std::copy(buffer,
+					  buffer + sizeof(value),
+					  span.Buffer());
+		}
 
-} // namespace cb::bit_converte
+	} // namespace bit_converte
+} // namespace cb
