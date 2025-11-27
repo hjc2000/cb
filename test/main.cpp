72e868e229faaa5feb674aa38f402ded6509f045
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-11-28 00:11:38 +0800

    ``` refactor(test): 移除IWYU pragma标记
    移除了main.cpp文件中不再需要的IWYU (Include What You Use)
    pragma标记，清理了不必要的编译器指令，使代码更加简洁。
    ```

--- test/main.cpp
+++ test/main.cpp
@@ -1,12 +1,8 @@
-// IWYU pragma: begin_keep
-
 #include "base/string/define.h"
 #include "base/string/ToHexString.h"
 #include "cb/math/cb_int64_fraction.h"
 #include <cstdint>
 #include <iostream>
-
-// IWYU pragma: end_keep
 
 int main()
 {
