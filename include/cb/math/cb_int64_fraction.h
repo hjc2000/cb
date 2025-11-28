dd569206a0b018be0e05158e10462382caf64d44
hjc2000 <hjc.666@foxmail.com>
2025-11-28 14:44:10 +0800

    ``` fix(cb_int64_fraction): 注释掉分数化简逻辑
    将 cb_int64_fraction 类中的分数化简代码暂时注释掉，
    可能由于该逻辑存在潜在问题或需要重新设计。
    后续需进一步确认是否恢复或替换此部分实现。
    ```

--- include/cb/math/cb_int64_fraction.h
+++ include/cb/math/cb_int64_fraction.h
@@ -92,20 +92,20 @@ namespace cb
 				return;
 			}
 
-			// 分子分母同时除以最大公约数
-			int64_t gcd_value = std::gcd(_num, _den);
-			int64_t scaled_num = _num / gcd_value;
-			int64_t scaled_den = _den / gcd_value;
+			// // 分子分母同时除以最大公约数
+			// int64_t gcd_value = std::gcd(_num, _den);
+			// int64_t scaled_num = _num / gcd_value;
+			// int64_t scaled_den = _den / gcd_value;
 
-			if (scaled_den < 0)
-			{
-				// 如果分母小于 0，分子分母同时取相反数，保证分母为正。
-				scaled_num = -scaled_num;
-				scaled_den = -scaled_den;
-			}
+			// if (scaled_den < 0)
+			// {
+			// 	// 如果分母小于 0，分子分母同时取相反数，保证分母为正。
+			// 	scaled_num = -scaled_num;
+			// 	scaled_den = -scaled_den;
+			// }
 
-			_num = scaled_num;
-			_den = scaled_den;
+			// _num = scaled_num;
+			// _den = scaled_den;
 		}
 
 		///
