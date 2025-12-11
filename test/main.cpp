9acecef6770f49cbdc265224215b583804d6f09c
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-12-11 23:20:39 +0800

    删除分数类的测试

--- test/main.cpp
+++ test/main.cpp
@@ -1,19 +1,9 @@
 #include "base/string/define.h"
 #include "base/string/ToHexString.h"
-#include "cb/math/Int64Fraction.h"
 #include <cstdint>
 #include <iostream>
 
 int main()
 {
-	constexpr uint64_t factor = static_cast<uint64_t>(1) << 30;
-	cb::Int64Fraction f1 = static_cast<int64_t>(cb::Int64Fraction{100, -3000} * factor);
-	std::cout << "f1 = " << f1.ToString() << std::endl;
-
-	f1 *= 3000;
-	f1 *= 10;
-	f1 /= factor;
-	std::cout << "f1 = " << f1.ToString() << std::endl;
-	std::cout << "static_cast<int64_t>(f1) = " << static_cast<int64_t>(f1) << std::endl;
 	return 0;
 }
